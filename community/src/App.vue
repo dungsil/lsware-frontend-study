877a88a4069ef487f85e7cb9f3dfcf88fea1e5a4
김용건 (Kim Younggeon) <mail@kyg.kr>
2020-01-14 21:04:08 +0900

    community: 로그인폼추가

--- community/src/App.vue
+++ community/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <router-view name="header" />
-
     <div class="content">
       <div class="card">
         <router-view />
--- community/src/pages/auth/Login.vue
+++ community/src/pages/auth/Login.vue
@@ -1,11 +1,34 @@
 <template>
     <div id="login">
       <h2>로그인</h2>
+
+      <form @submit.prevent="onLogin">
+        <label for="email">
+          <input id="email" type="text" v-model="email" placeholder="이메일을 입력해주세요" />
+        </label>
+        <label for="password">
+          <input id="password" type="password" v-model="password" placeholder="비밀번호 를 입력해주세요" />
+        </label>
+
+        <input type="submit" value="로그인">
+      </form>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onLogin () {
+      const { email, password } = this
+      console.log(email, password)
+    }
+  }
 }
 </script>
